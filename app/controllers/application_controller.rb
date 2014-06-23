6c462305565c38bf4e6481f584268154956ec330
Felipe Luna <pancreasnou@gmail.com>
2014-06-23 14:40:51 -0300

    >> form cadastro add data_nascimento.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
 
  # linha 3- 12 > add dados no banco de dados.
- 	
+
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+  	# para salvar no banco de dados.
+    def configure_permitted_parameters                        # atributos para salvar no banco de dados
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :data, :email, :password, :password_confirmation) }
       # you control which attributes can be updated or used for sign in here
     end
 end
