f490b43f656a5e00b90669d085ee8f223aee1a5e
Felipe Luna <pancreasnou@gmail.com>
2014-06-24 18:55:31 -0300

    >> editando nome do usuario, link para editar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   	# para salvar no banco de dados.
     def configure_permitted_parameters                        # atributos para salvar no banco de dados
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :data, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
       # you control which attributes can be updated or used for sign in here
     end
 end
